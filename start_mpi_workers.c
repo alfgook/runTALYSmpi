4644af6eada222025ad2c0c5fa366f7761fb4677
alfgook <alg.gook@physics.uu.se>
2022-04-26 14:52:56 +0200

    test

--- start_mpi_workers.c
+++ start_mpi_workers.c
@@ -86,14 +86,14 @@ int start_mpi_workers(const char **worker_program, char *job_list[], const int *
                      MPI_ERRCODES_IGNORE);
 */
 
-   MPI_Comm_spawn(worker_program[0],
+   /*MPI_Comm_spawn(worker_program[0],
                      argv,
                      nbr_of_workers,
                      MPI_INFO_NULL, 
                      0,
                      MPI_COMM_SELF,
                      &everyone,
-                     MPI_ERRCODES_IGNORE);
+                     MPI_ERRCODES_IGNORE);*/
    
    // Could add parallel code here. The communicator "everyone" can be used to communicate with
    // the spawned processes, which have ranks 0,.. nbr_of_workers-1 in the remote group of 
@@ -102,11 +102,11 @@ int start_mpi_workers(const char **worker_program, char *job_list[], const int *
    //MPI_Barrier(everyone);
 
    printf("workers spawned... waiting...");
-   for(int worker=0;worker<nbr_of_workers;worker++) {
+   /*for(int worker=0;worker<nbr_of_workers;worker++) {
       int rank;
       MPI_Recv(&rank, 1, MPI_INT, worker, 0, everyone, MPI_STATUS_IGNORE);
       //printf("master received %d from worker %d\n",rank,worker);
-   }
+   }*/
    
 
    free(argv);
